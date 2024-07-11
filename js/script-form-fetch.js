aeda95b43212c2dab4f9f8c6abdcbe6c43d4b505
Mbel99 <99453102+Mbel99@users.noreply.github.com>
2024-07-11 19:59:03 -0300

    Update script-form-fetch.js

--- js/script-form-fetch.js
+++ js/script-form-fetch.js
@@ -1,6 +1,6 @@
 //const BASEURL ='http://127.0.0.1:5000/';
 
-const BASEURL='https://paginaturismo.pythonanywhere.com/api/contactos/'
+const BASEURL='https://paginaturismo.pythonanywhere.com/'
 
 /**
  * Función para realizar una petición fetch con JSON.
